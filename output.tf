819dd0013a42c991f5eeba206ed5f61caef29a9e
Ayman Zahran <ah.zahran@outlook.com>
2020-07-07 01:30:58 +0200

    2nd Commit

--- output.tf
+++ output.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "PRIVATE_IP" {
     value = aws_instance.example.private_ip
     description = "The private IP address of the instance."
@@ -8,14 +7,3 @@ output "PUBLIC_IP" {
     value = aws_instance.example.public_ip
     description = "The public IP address of the instance."
 }
-=======
-output "PRIVATE_IP" {
-    value = aws_instance.example.private_ip
-    description = "The private IP address of the instance."
-}
-
-output "PUBLIC_IP" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the instance."
-}
->>>>>>> d1d90ae3c726736ba8e39a7899f6d391e2e58a2c
