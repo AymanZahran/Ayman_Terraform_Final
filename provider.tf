204f82004f4d1132ba3971c582e3c40723f1f74c
Ayman Zahran <ah.zahran@outlook.com>
2020-07-07 03:49:31 +0200

    Last Commit

--- provider.tf
+++ provider.tf
@@ -1,30 +1,26 @@
-# provider "aws" {
-#   access_key = var.AWS_ACCESS_KEY
-#   secret_key = var.AWS_SECRET_KEY
-#   region     = var.AWS_REGION
-# }
+#AWS Provider
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+}
 
+#Azure Provider
 provider "azurerm" {
     version = "~>1.32.0"
     use_msi = true
-    subscription_id = "210dc5f0-273e-4029-8547-aff58db30d98"
-    #var.AZURE_SUBSCRIPTION_IT
-    tenant_id       = "3e5e67ad-2e8c-4a0a-b170-8fe996eebec0"
-    #var.AZURE_TENANT_ID
-    client_id       = "ff9f2cd0-a91d-4f34-97ac-6670bb08cc05"
-    #var.AZURE_CLIENT_ID
-    client_secret   = "W_A_7xBymjd5-2mk_n9802qHDlXBD-Xo7B"
-    #var.AZURE_CLIENT_SECRET
+    subscription_id = var.AZURE_SUBSCRIPTION_ID
+    tenant_id       = var.AZURE_TENANT_ID
+    client_id       = var.AZURE_CLIENT_ID
+    client_secret   = var.AZURE_CLIENT_SECRET
 }
 
-
+#GCP Provider
 provider "google" {
   version = "3.5.0"
-
   credentials = file("<NAME>.json")
-
-  project = "<PROJECT_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.GCP_PROJECT_ID
+  region  = var.GCP_REGION
+  zone    = var.GCP_ZONE
 }
 
--- vars.tf
+++ vars.tf
@@ -1,32 +1,45 @@
-# variable "AWS_ACCESS_KEY" {}
+#AWS Variables
+variable "AWS_ACCESS_KEY" {}
 
-# variable "AWS_SECRET_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
-# variable "AWS_REGION" {
-#   default = "eu-west-1"
-# }
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
 
-# variable "AWS_INSTANCE_TYPE" {
-#   default = "t2.micro"
-# }
+variable "AWS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
 
-# variable "AMIS" {
-#   type = map(string)
-#   default = {
-#       us-east-1 = "ami-13be557e"
-#       us-west-2 = "ami-06b94666"
-#       eu-west-1 = "ami-0d729a60"
-#     }
-# }
+variable "AMIS" {
+  type = map(string)
+  default = {
+      us-east-1 = "ami-13be557e"
+      us-west-2 = "ami-06b94666"
+      eu-west-1 = "ami-0d729a60"
+    }
+}
 
-# variable "AZURE_SUBSCRIPTION_ID" {}
+#Azure Variables
+variable "AZURE_SUBSCRIPTION_ID" {}
 
-# variable "AZURE_TENANT_ID" {}
+variable "AZURE_TENANT_ID" {}
 
-# variable "AZURE_CLIENT_ID" {}
+variable "AZURE_CLIENT_ID" {}
 
-# variable "AZURE_CLIENT_SECRET" {}
+variable "AZURE_CLIENT_SECRET" {}
 
 variable "AZURE_REGION" {
   default = "westus"
+}
+
+#GCP Variables
+variable "GCP_PROJECT_ID" {}
+
+variable "GCP_REGION" {
+  default = "us-central1"
+}
+
+variable "GCP_ZONE" {
+  default = "us-central1-c"
 }
