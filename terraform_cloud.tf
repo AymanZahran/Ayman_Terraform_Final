f49931cb538ea511331e058b0e12d90a17dd5ed3
Ayman Zahran <ah.zahran@outlook.com>
2020-07-07 02:00:56 +0200

    Commit11

--- terraform_cloud.tf
+++ terraform_cloud.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Using a single workspace:
 terraform {
   backend "remote" {
@@ -11,17 +10,3 @@ terraform {
   }
 }
 
-=======
-# Using a single workspace:
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "Ayman_Terraform_Final"
-
-    workspaces {
-      name = "Terraform_Final"
-    }
-  }
-}
-
->>>>>>> d1d90ae3c726736ba8e39a7899f6d391e2e58a2c
