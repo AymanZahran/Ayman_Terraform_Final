88c38412105e1c1f3d17650a3de7f1830437aa1e
Ayman Zahran <ah.zahran@outlook.com>
2020-07-07 03:51:25 +0200

    Commit2

---
+++ Resources.tf
@@ -0,0 +1,16 @@
+#AWS Resources
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = var.AWS_INSTANCE_TYPE
+}
+
+#Azure Resources
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroup"
+    location = var.AZURE_REGION
+}
+
+#GCP Resources
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
--- instance.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_instance" "example" {
-#   ami           = var.AMIS[var.AWS_REGION]
-#   instance_type = var.AWS_INSTANCE_TYPE
-# }
-
-resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
-    location = var.AZURE_REGION
-}
