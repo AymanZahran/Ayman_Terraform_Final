c697c57e593db81864c95d0ba37ee93f4ff842b1
Ayman Zahran <ah.zahran@outlook.com>
2020-07-15 10:09:40 +0200

    Commit

--- Providers.tf
+++ Providers.tf
@@ -10,8 +10,8 @@ provider "azurerm" {
     use_msi = true
     subscription_id = "d855b6ab-0588-47f5-9095-3315e5745db0"
     tenant_id       = "ce1acd89-9604-42e5-b501-a3f292efe44d"
-    #client_id       = "653b057f-56d0-456b-9d19-1ddc80181102"
-    #client_secret   = "N6~eoc470yFzwjp3DMY4y4mu_l~-_3Vpr~"
+    client_id       = "653b057f-56d0-456b-9d19-1ddc80181102"
+    client_secret   = "N6~eoc470yFzwjp3DMY4y4mu_l~-_3Vpr~"
     features{}
 }
 
