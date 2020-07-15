85d7c6ab695c25e30c71a63d121e9307ea28182e
Ayman Zahran <ah.zahran@outlook.com>
2020-07-15 09:34:07 +0200

    Commit All

--- Providers.tf
+++ Providers.tf
@@ -13,7 +13,6 @@ provider "azurerm" {
     tenant_id       = var.AZURE_TENANT_ID
     #client_id       = var.AZURE_CLIENT_ID
     #client_secret   = var.AZURE_CLIENT_SECRET
-    features {}
 }
 
 #GCP Provider
