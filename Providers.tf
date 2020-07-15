bcc9e2d30e3c5b16f4d5268b888514d2be265bb3
Ayman Zahran <ah.zahran@outlook.com>
2020-07-15 10:10:56 +0200

    Commit All

--- Providers.tf
+++ Providers.tf
@@ -7,12 +7,12 @@
 
 #Azure Provider
 provider "azurerm" {
+    version = "~>1.32.0"
     use_msi = true
-    subscription_id = "d855b6ab-0588-47f5-9095-3315e5745db0"
-    tenant_id       = "ce1acd89-9604-42e5-b501-a3f292efe44d"
-    client_id       = "653b057f-56d0-456b-9d19-1ddc80181102"
-    client_secret   = "N6~eoc470yFzwjp3DMY4y4mu_l~-_3Vpr~"
-    features{}
+    subscription_id = var.AZURE_SUBSCRIPTION_ID
+    tenant_id       = var.AZURE_TENANT_ID
+    #client_id       = var.AZURE_CLIENT_ID
+    #client_secret   = var.AZURE_CLIENT_SECRET
 }
 
 #GCP Provider
