edcf22dc2167c036e584c44e4997732ffcf8ccdb
Ayman Zahran <ah.zahran@outlook.com>
2020-09-05 17:03:00 +0200

    Today Commit

--- Providers.tf
+++ Providers.tf
@@ -1,26 +1,34 @@
-#AWS Provider
-# provider "aws" {
-#   access_key = var.AWS_ACCESS_KEY
-#   secret_key = var.AWS_SECRET_KEY
-#   region     = var.AWS_REGION
-# }
-
-Azure Provider
 provider "azurerm" {
-    version = "~>1.32.0"
-    use_msi = true
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
     subscription_id = var.AZURE_SUBSCRIPTION_ID
-    tenant_id       = var.AZURE_TENANT_ID
-    #client_id       = var.AZURE_CLIENT_ID
-    #client_secret   = var.AZURE_CLIENT_SECRET
 }
 
-#GCP Provider
-# provider "google" {
-#   version = "3.5.0"
-#   credentials = file("<NAME>.json")
-#   project = var.GCP_PROJECT_ID
-#   region  = var.GCP_REGION
-#   zone    = var.GCP_ZONE
+# #AWS Provider
+# # provider "aws" {
+# #   access_key = var.AWS_ACCESS_KEY
+# #   secret_key = var.AWS_SECRET_KEY
+# #   region     = var.AWS_REGION
+# # }
+
+# Azure Provider
+# provider "azurerm" {
+#     version = "~>1.32.0"
+#     use_msi = true
+#     subscription_id = var.AZURE_SUBSCRIPTION_ID
+#     tenant_id       = var.AZURE_TENANT_ID
+#     #client_id       = var.AZURE_CLIENT_ID
+#     #client_secret   = var.AZURE_CLIENT_SECRET
 # }
+
+# #GCP Provider
+# # provider "google" {
+# #   version = "3.5.0"
+# #   credentials = file("<NAME>.json")
+# #   project = var.GCP_PROJECT_ID
+# #   region  = var.GCP_REGION
+# #   zone    = var.GCP_ZONE
+# # }
 
--- vars.tf
+++ vars.tf
@@ -1,24 +1,24 @@
-#AWS Variables
-variable "AWS_ACCESS_KEY" {}
+# #AWS Variables
+# variable "AWS_ACCESS_KEY" {}
 
-variable "AWS_SECRET_KEY" {}
+# variable "AWS_SECRET_KEY" {}
 
-variable "AWS_REGION" {
-  default = "us-east-1"
-}
+# variable "AWS_REGION" {
+#   default = "us-east-1"
+# }
 
-variable "AWS_INSTANCE_TYPE" {
-  default = "t2.micro"
-}
+# variable "AWS_INSTANCE_TYPE" {
+#   default = "t2.micro"
+# }
 
-variable "AWS_AMIS" {
-  type = map(string)
-  default = {
-      us-east-1 = "ami-13be557e"
-      us-west-2 = "ami-06b94666"
-      eu-west-1 = "ami-0d729a60"
-    }
-}
+# variable "AWS_AMIS" {
+#   type = map(string)
+#   default = {
+#       us-east-1 = "ami-13be557e"
+#       us-west-2 = "ami-06b94666"
+#       eu-west-1 = "ami-0d729a60"
+#     }
+# }
 
 #Azure Variables
 # variable "AZURE_SUBSCRIPTION_ID" {}
